dad55bde3fabc0919d030a783aa3bc3cc3ea4225
nisatnilofficial0271 <107569069+nisatnilofficial0271@users.noreply.github.com>
2023-03-14 12:04:02 +0600

    Update MAHADI-PRO.py

--- MAHADI-PRO.py
+++ MAHADI-PRO.py
@@ -1,14 +1,1 @@
-import os, platform
-try:
-    import requests
-except:
-    os.system('pip2 install requests')
-
-import requests
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from PRO import Subscraption
-    Subscraption()
-elif bit == '32bit':
-    from mahadi import menu
-    menu()
+This Is Nisat Bro
